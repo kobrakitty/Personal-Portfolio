d331038e7a1e67f06ec93f8afd51cfb778ec280e
kobrakitty <shelbywentz@gmail.com>
2024-08-19 23:14:05 -0700

    Update settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -133,5 +133,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shelbywentz@gmail.com'
-EMAIL_HOST_PASSWORD ='tiij nrnv xmee kjwj'
