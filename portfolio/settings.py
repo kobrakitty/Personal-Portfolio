3ea68924a93a28cd2472c2639b87265895003039
kobrakitty <shelbywentz@gmail.com>
2024-08-21 10:19:12 -0700

    Static Files fix
    with one debug test. Testing deploy.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -63,7 +63,7 @@ if not os.getenv('DB_URL'):
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
+DEBUG = True
 
 # Application definition
 
@@ -155,31 +155,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Media processing
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Tell Django to copy static assets into a path called `static` (this is specific to Render)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'Django Workshop' / 'static')
+
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
-STATIC_URL = '/static/'
-
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-# old or duplicate code below; keeping in case Render doc doesn't work
-#STATIC_URL = '/static/'
-#STATIC_ROOT = BASE_DIR / 'portfolio' / 'staticfiles'
 #STATICFILES_DIRS = [
-   # BASE_DIR / 'static',
-   # BASE_DIR / 'portfolio' / 'static',
-   # BASE_DIR / 'media' / 'static',
+  #BASE_DIR / 'static',
+   #BASE_DIR / 'portfolio' / 'static',
+   #BASE_DIR / 'media' / 'static',
 #]
-
-#Media processing
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
